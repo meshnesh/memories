27550ce5b641dd662a20b6b26ed597d7a4ab904c
meshnesh <tonny.nesh@gmail.com>
2016-07-08 12:35:16 +0300

    added splash screen

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-$(document).foundation()
+$(document).foundation();
+var element = document.getElementById("imageSplash");
+element.addEventListener("mousewheel", MouseWheelHandler, false);
+element.addEventListener("DOMMouseScroll", MouseWheelHandler, false);
+
+function MouseWheelHandler(e) {
+    document.getElementById("clickScroll").checked = true;
+    return false;
+}
